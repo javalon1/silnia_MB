5ffe92179156e79fd45dea21d9475d5dc790a577
bajserowiczMikolaj <mbajserowicz@gmail.com>
2019-05-11 15:40:16 +0100

    rekurencja

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -4,10 +4,45 @@ package com.sda.algorytmy;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static double ciag(int n){
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 0.5;
+        }
+        return -ciag(n-1) * ciag(n-2);
+    }
+
+
+    public static int ciag2(int n){
+
+        if(n==1){
+        return -1;
+        }
+       return -ciag2(n-1) * n -3;
+    }
+
+    public static int silnia(int n){
+        System.out.println(n);
+
+        if(n<2){
+            return 1;
+        }
+        return n * silnia(n-1);
+    }
+
+    public static void main( String[] args ) {
+
+        silnia(10);
+
+        int silnia = 5;
+        int wynik = 1;
+        for (int i=1; i<=silnia; i++){
+            wynik = wynik * i;
+        }
+        System.out.printf("silnia: " + wynik );
     }
 }
